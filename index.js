9970f3e8ffecb532d7ed2470afbb3c8c0857cb05
Quanbeothoima123 <phimanhh85@gmail.com>
2025-06-05 23:51:17 +0700

    update admin dashboard

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ require("dotenv").config();
 
 const database = require("./config/database");
 
+const systemConfig = require("./config/system");
+
+const routeAdmin = require("./routes/admin/index.route");
 const route = require("./routes/client/index.route");
 
 database.connect();
@@ -15,9 +18,12 @@ const port = process.env.PORT;
 app.set("views", "./views");
 app.set("view engine", "pug");
 
+//APP locals variable
+app.locals.prefixAdmin = systemConfig.prefixAdmin;
+
 app.use(express.static("public"));
 // Routes
-
+routeAdmin(app);
 route(app);
 
 app.listen(port, () => {
