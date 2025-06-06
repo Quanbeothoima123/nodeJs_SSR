55a05340be015d24314f9de147b66677c5f76578
Quanbeothoima123 <phimanhh85@gmail.com>
2025-06-07 01:17:25 +0700

    update

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,37 @@
-console.log("OK");
+//Button Status
+const buttonsStatus = document.querySelectorAll("[button-status]");
+if (buttonsStatus.length > 0) {
+  let url = new URL(window.location.href);
+  console.log(url);
+  buttonsStatus.forEach((button) => {
+    button.addEventListener("click", () => {
+      const status = button.getAttribute("button-status");
+
+      if (status) {
+        url.searchParams.set("status", status);
+      } else {
+        url.searchParams.delete("status");
+      }
+      window.location.href = url;
+    });
+  });
+}
+// End Button Status
+
+//Form search
+const formSearch = document.querySelector("#form-search");
+if (formSearch) {
+  let url = new URL(window.location.href);
+
+  formSearch.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const keyword = e.target.elements.keyword.value;
+    if (keyword) {
+      url.searchParams.set("keyword", keyword);
+    } else {
+      url.searchParams.delete("keyword");
+    }
+    window.location.href = url.href;
+  });
+}
+//End form search
