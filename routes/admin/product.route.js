3245a13cb6b83c0f0232fa08313ac1d2566ae533
Quanbeothoima123 <phimanhh85@gmail.com>
2025-06-16 13:25:57 +0700

    upload anh vao thu muc uploads va luu duong link anh san pham vao database dung multer

--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -1,10 +1,16 @@
 const express = require("express");
+
+const multer = require("multer");
 const router = express.Router();
+
+const storageMulter = require("../../helper/storageMulter");
+const upload = multer({ storage: storageMulter() }); // dest  là thư mục root của dự án
+
 const controller = require("../../controllers/admin/product.controller");
 router.get("/", controller.product);
 router.patch("/change-status/:status/:id", controller.changeStatus);
 router.patch("/change-multi", controller.changeMulti);
 router.delete("/delete/:id", controller.deleteItem);
 router.get("/create", controller.create);
-router.post("/create", controller.createPost);
+router.post("/create", upload.single("thumbnail"), controller.createPost);
 module.exports = router;
