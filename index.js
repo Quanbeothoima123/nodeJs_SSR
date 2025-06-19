3ed89217c78a88838640995800283ea5da1c79f2
Quanbeothoima123 <phimanhh85@gmail.com>
2025-06-19 10:15:13 +0700

    config vercel.json de push len online

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use(methodOverride("_method"));
 //parse application /x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.set("views", "./views");
+app.set("views", `${__dirname}/views`);
 app.set("view engine", "pug");
 
 //Flash
@@ -43,7 +43,7 @@ app.use(flash());
 //APP locals variable
 app.locals.prefixAdmin = systemConfig.prefixAdmin;
 
-app.use(express.static("public"));
+app.use(express.static(`${__dirname}/public`));
 // Routes
 routeAdmin(app);
 route(app);
