0af5f6ffa1c56a5734506fefe3261944ba781068
Quanbeothoima123 <phimanhh85@gmail.com>
2025-07-08 23:19:39 +0700

    Cập nhật tính năng tìm kiếm sản phẩm dùng tên sản phẩm

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,8 +1,10 @@
 const categoryMiddleware = require("../../middlewares/client/category.middleware");
 const productRouters = require("./product.route");
 const homeRouters = require("./home.route");
+const searchRouters = require("./search.route");
 module.exports = (app) => {
   app.use(categoryMiddleware.category);
   app.use("/", homeRouters);
   app.use("/products", productRouters);
+  app.use("/search", searchRouters);
 };
